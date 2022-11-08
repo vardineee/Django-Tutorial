28c9d66147fa61510743393c28fe19eec3c254a4
Varduhi Poghosyan <83503972+vardineee@users.noreply.github.com>
2022-11-08 23:07:15 +0400

    Update views.py

--- modelDemo/empApp/views.py
+++ modelDemo/empApp/views.py
@@ -4,6 +4,5 @@ from empApp import models
 # Create your views here.
 def employeedata(request):
     employees = models.Employee.objects.all()
-    # print(employees)
     empDict = {"employees":employees}
     return render (request,'empApp/emp.html', empDict)
